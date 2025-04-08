d31bb6ea2d5d59a952c15c1caf5553cfd3848f75
mgm1221 <martingarcesm@gmail.com>
2025-04-08 15:18:38 -0400

    Agregado de funcion run, no necesario de tdd

--- kata.py
+++ kata.py
@@ -2,9 +2,19 @@ class Kata:
     def __init__(self,name):
         self.name = name
         print("Hola " + self.name)
+        self.run()
     
     def input(self,message):
         if message != "Stop!":
             return message[::-1]
         else:
             return "Adios " + self.name
+    
+    def run(self):
+        var = True
+        while var:
+            message = input()
+            result = self.input(message)
+            if result == "Adios " + self.name:
+                var = False
+            print(result)
